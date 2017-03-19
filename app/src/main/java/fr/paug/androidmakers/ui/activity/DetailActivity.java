261070a9b731d7b0f0e972fbf9bcd01c21872612
Adrien Vitti <adrien@smartnsoft.com>
2017-03-19 20:49:08 +0100

    Began to work on detail session view

--- app/src/main/java/fr/paug/androidmakers/ui/activity/DetailActivity.java
+++ app/src/main/java/fr/paug/androidmakers/ui/activity/DetailActivity.java
@@ -5,38 +5,82 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import fr.paug.androidmakers.R;
 import fr.paug.androidmakers.manager.AgendaRepository;
 import fr.paug.androidmakers.model.Session;
+import fr.paug.androidmakers.model.Speaker;
 
 /**
  * Created by stan on 19/03/2017.
  */
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity
+    extends AppCompatActivity
+{
 
-    private static final String PARAM_SESSION_ID = "param_session_id";
+  private static final String PARAM_SESSION_ID = "param_session_id";
 
-    public static void startActivity(Context context, int sessionId) {
-        Intent intent = new Intent(context, DetailActivity.class);
-        intent.putExtra(PARAM_SESSION_ID, sessionId);
-        context.startActivity(intent);
+  public static void startActivity(Context context, int sessionId)
+  {
+    Intent intent = new Intent(context, DetailActivity.class);
+    intent.putExtra(PARAM_SESSION_ID, sessionId);
+    context.startActivity(intent);
+  }
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_detail);
+
+    final Session session = AgendaRepository.getInstance().getSession(
+        getIntent().getIntExtra(PARAM_SESSION_ID, -1)
+    );
+
+    if (session == null)
+    {
+      // We have a problem !
+      final TextView errorMessage = (TextView) findViewById(R.id.errorMessage);
+      final View sessionInformations = findViewById(R.id.sessionInformations);
+      sessionInformations.setVisibility(View.GONE);
+      errorMessage.setVisibility(View.VISIBLE);
+      return;
     }
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    final TextView sessionTitle = (TextView) findViewById(R.id.sessionTitle);
+    final TextView sessionDescription = (TextView) findViewById(R.id.sessionDescription);
+    final TextView sessionLanguage = (TextView) findViewById(R.id.sessionLanguage);
+    final TextView sessionType = (TextView) findViewById(R.id.sessionType);
 
-        setContentView(R.layout.activity_detail);
+    sessionTitle.setText(session.title);
+    sessionDescription.setText(Html.fromHtml(session.description));
+    sessionLanguage.setText(session.language);
+    sessionType.setText(session.subtype);
 
-        TextView textView = (TextView) findViewById(R.id.title_textview);
+    final ViewGroup sessionSpeakerLayout = (ViewGroup) findViewById(R.id.sessionSpeakerLayout);
+    if (session.speakers != null && session.speakers.length > 0)
+    {
+      for (int speakerID : session.speakers)
+      {
+        final Speaker speaker = AgendaRepository.getInstance().getSpeaker(speakerID);
+        if (speaker != null)
+        {
+          final View speakerView = getLayoutInflater().inflate(R.layout.detail_view_speaker_info_element, null, false);
+          final ImageView speakerAvatar = (ImageView) speakerView.findViewById(R.id.speakerAvatar);
+          final TextView speakerNameAndCompany = (TextView) speakerView.findViewById(R.id.speakerName);
+          final TextView speakerBio = (TextView) speakerView.findViewById(R.id.speakerBio);
 
-        Session session = AgendaRepository.getInstance().getSession(
-                getIntent().getIntExtra(PARAM_SESSION_ID, -1)
-        );
-
-        textView.setText(session == null ? "Session not found" : session.title);
+          speakerNameAndCompany.setText(speaker.name + " " + speaker.surname + ", " + speaker.company);
+          speakerBio.setText(Html.fromHtml(speaker.bio));
+          sessionSpeakerLayout.addView(speakerView);
+        }
+      }
     }
+  }
 }
