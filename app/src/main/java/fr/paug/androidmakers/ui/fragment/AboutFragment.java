0c64687980d12e9136af5c83fa11f208a748348d
benju69 <begonin@gmail.com>
2017-03-23 20:48:02 +0100

    Fix

--- app/src/main/java/fr/paug/androidmakers/ui/fragment/AboutFragment.java
+++ app/src/main/java/fr/paug/androidmakers/ui/fragment/AboutFragment.java
@@ -56,7 +56,7 @@ public class AboutFragment extends Fragment {
         try {
             // get the Twitter app if possible
             getActivity().getPackageManager().getPackageInfo("com.twitter.android", 0);
-            twitterIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("witter://search?query=%23AndroidMakersFR"));
+            twitterIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("twitter://search?query=%23AndroidMakersFR"));
             twitterIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         } catch (Exception e) {
             // no Twitter app, revert to browser
