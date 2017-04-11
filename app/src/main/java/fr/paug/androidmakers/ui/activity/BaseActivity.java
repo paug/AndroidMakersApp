a753c65f81ee6cb281f3f341b6d5c177b71826e3
Cyril Mottier <cyril@cyrilmottier.com>
2017-04-11 09:37:37 +0200

    Enhance the launching experience with a placeholder UI

--- app/src/main/java/fr/paug/androidmakers/ui/activity/BaseActivity.java
+++ app/src/main/java/fr/paug/androidmakers/ui/activity/BaseActivity.java
@@ -3,11 +3,13 @@ package fr.paug.androidmakers.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import fr.paug.androidmakers.util.ThemeUtils;
 
 public class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        ThemeUtils.ensureRuntimeTheme(this);
         super.onCreate(savedInstanceState);
     }
 
