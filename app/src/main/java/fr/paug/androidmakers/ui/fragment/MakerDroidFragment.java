785542d662dd44fae6ca9f31d292d1e40379414b
jadecdev <chabaro.j@sfeir-ext.com>
2018-03-25 16:45:06 +0200

    view methods

--- app/src/main/java/fr/paug/androidmakers/ui/fragment/MakerDroidFragment.java
+++ app/src/main/java/fr/paug/androidmakers/ui/fragment/MakerDroidFragment.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
@@ -19,6 +21,8 @@ import android.widget.Toast;
 import com.google.gson.JsonElement;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Formatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -36,8 +40,12 @@ import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import fr.paug.androidmakers.R;
 import fr.paug.androidmakers.manager.AgendaRepository;
+import fr.paug.androidmakers.model.Room;
 import fr.paug.androidmakers.model.ScheduleSlot;
 import fr.paug.androidmakers.model.Session;
+import fr.paug.androidmakers.model.Speaker;
+import fr.paug.androidmakers.ui.activity.DetailActivity;
+import fr.paug.androidmakers.ui.view.AgendaView;
 
 /**
  * Created by Jade on 20/03/2018
@@ -72,7 +80,8 @@ public class MakerDroidFragment extends Fragment implements AIListener {
     private String[] permissions = {Manifest.permission.RECORD_AUDIO};
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
 
-    private int paddingDp;
+    private int padding;
+    private int paddingDouble;
     private LinearLayout.LayoutParams paramsQuestion;
     private LinearLayout.LayoutParams paramsAnswer;
 
@@ -108,22 +117,23 @@ public class MakerDroidFragment extends Fragment implements AIListener {
             }
         });
 
-
-        paddingDp = (int) getResources().getDimension(R.dimen.padding);
+        padding = (int) getResources().getDimension(R.dimen.padding);
+        paddingDouble = (int) getResources().getDimension(R.dimen.padding_double);
 
         paramsQuestion = new LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.WRAP_CONTENT,
             LinearLayout.LayoutParams.WRAP_CONTENT);
         paramsQuestion.weight = 1.0f;
         paramsQuestion.gravity = Gravity.RIGHT;
-        paramsQuestion.setMargins(0, paddingDp, 0, 0);
+        paramsQuestion.setMargins(paddingDouble, 0, paddingDouble, paddingDouble);
 
         paramsAnswer = new LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.WRAP_CONTENT,
             LinearLayout.LayoutParams.WRAP_CONTENT);
         paramsAnswer.weight = 1.0f;
         paramsAnswer.gravity = Gravity.LEFT;
-        paramsAnswer.setMargins(0, paddingDp, 0, 0);
+        paramsAnswer.setMargins(paddingDouble, 0, paddingDouble, paddingDouble);
+
 
         return view;
     }
@@ -167,7 +177,6 @@ public class MakerDroidFragment extends Fragment implements AIListener {
                 Log.i(TAG, "Intent id: " + metadata.getIntentId());
                 Log.i(TAG, "Intent name: " + metadata.getIntentName());
             }
-
 
             addQuestionView(result.getResolvedQuery());
 
@@ -245,25 +254,32 @@ public class MakerDroidFragment extends Fragment implements AIListener {
 
     private void addQuestionView(String text) {
         TextView tvQuestion = new TextView(this.getContext());
+        tvQuestion.setFocusable(true);
+        tvQuestion.setFocusableInTouchMode(true);
         tvQuestion.setText(text);
         tvQuestion.setBackgroundDrawable(getResources().getDrawable(R.drawable.bg_bot_question));
         tvQuestion.setLayoutParams(paramsQuestion);
-        tvQuestion.setPadding(paddingDp, paddingDp, paddingDp, paddingDp);
+        tvQuestion.setPadding(padding, padding, padding, padding);
         botLayout.addView(tvQuestion);
+        tvQuestion.requestFocus();
     }
 
     private void addAnswerView(String text) {
         TextView tvAnswer = new TextView(this.getContext());
+        tvAnswer.setFocusable(true);
+        tvAnswer.setFocusableInTouchMode(true);
         tvAnswer.setText(text);
         tvAnswer.setBackgroundDrawable(getResources().getDrawable(R.drawable.bg_bot_answer));
         tvAnswer.setLayoutParams(paramsAnswer);
-        tvAnswer.setPadding(paddingDp, paddingDp, paddingDp, paddingDp);
+        tvAnswer.setPadding(padding, padding, padding, padding);
         botLayout.addView(tvAnswer);
+        tvAnswer.requestFocus();
+
     }
 
     private String treatQuestion(HashMap<String, JsonElement> parameters) {
 
-        List<Session> resultSessions = new ArrayList<>();
+        List<ScheduleSlot> resultSessions = new ArrayList<>();
 
         try {
             if (parameters.get("language") != null) {
@@ -274,12 +290,15 @@ public class MakerDroidFragment extends Fragment implements AIListener {
                     if (session != null &&
                         session.language != null &&
                         getResources().getString(Session.getLanguageFullName(session.language)).equalsIgnoreCase(userLang)) {
-                        resultSessions.add(session);
+                        resultSessions.add(slot);
                     }
                 }
 
                 String returnedMsg = "I have " + resultSessions.size() + "/" + AgendaRepository.getInstance().getScheduleSlots().size() + " sessions " +
                     "in " + userLang;
+
+//                addCarouselView(resultSessions);
+//                addListView(resultSessions);
 
                 Log.d(TAG, returnedMsg);
                 return returnedMsg;
@@ -291,5 +310,97 @@ public class MakerDroidFragment extends Fragment implements AIListener {
             Log.e(TAG, e.getMessage());
             return "An error occurred, please try again.";
         }
+    }
+
+    private void addCarouselView(List<ScheduleSlot> slots) {
+
+        LinearLayout.LayoutParams defaultWrapParams = new LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        HorizontalScrollView hScrollView = new HorizontalScrollView(getContext());
+        hScrollView.setLayoutParams(defaultWrapParams);
+        hScrollView.setPadding(0,0,0,paddingDouble);
+
+        LinearLayout ll = new LinearLayout(getContext());
+        ll.setOrientation(LinearLayout.HORIZONTAL);
+        ll.setLayoutParams(defaultWrapParams);
+        ll.setPadding(paddingDouble, 0, paddingDouble, 0);
+
+
+
+        LinearLayout.LayoutParams viewParams = new LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT);
+        viewParams.setMargins(padding, 0, padding, 0);
+
+
+        for (ScheduleSlot slot : slots) {
+            Session session = AgendaRepository.getInstance().getSession(slot.sessionId);
+            TextView tvSession = new TextView(this.getContext());
+            tvSession.setText(session != null ? session.title : "No session");
+            tvSession.setBackgroundDrawable(getResources().getDrawable(R.drawable.bg_bot_answer));
+            tvSession.setLayoutParams(viewParams);
+            ll.addView(tvSession);
+        }
+
+        hScrollView.addView(ll);
+        botLayout.addView(hScrollView);
+
+    }
+
+    private void addListView(List<ScheduleSlot> slots) {
+
+        LinearLayout.LayoutParams defaultWrapParams = new LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT);
+
+
+        LinearLayout ll = new LinearLayout(getContext());
+        ll.setOrientation(LinearLayout.VERTICAL);
+        ll.setLayoutParams(defaultWrapParams);
+        ll.setPadding(paddingDouble, 0, paddingDouble, paddingDouble);
+
+
+        LinearLayout.LayoutParams contentParams = new LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        for (final ScheduleSlot slot : slots) {
+            final Session session = AgendaRepository.getInstance().getSession(slot.sessionId);
+            final Room sessionRoom = AgendaRepository.getInstance().getRoom(slot.room);
+
+            TextView tvSession = new TextView(this.getContext());
+
+            final String sessionDate = DateUtils.formatDateRange(
+                getContext(),
+                new Formatter(getResources().getConfiguration().locale),
+                slot.startDate,
+                slot.endDate,
+                DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_ABBREV_WEEKDAY | DateUtils.FORMAT_SHOW_TIME,
+                null).toString();
+
+
+            tvSession.setText(
+                "Title: " + (session != null ? session.title : "No session") +
+                "\nSubtype: " + session.subtype +
+                "\nRoom: " + sessionRoom.name +
+                "\nDate: " + sessionDate
+            );
+
+            tvSession.setBackgroundDrawable(getResources().getDrawable(R.drawable.bg_bot_list));
+            tvSession.setLayoutParams(contentParams);
+            tvSession.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AgendaView.Item item = new AgendaView.Item(slot, session.title);
+                    DetailActivity.startActivity(getActivity(), item);
+                }
+            });
+            ll.addView(tvSession);
+        }
+
+        botLayout.addView(ll);
+
     }
 }
