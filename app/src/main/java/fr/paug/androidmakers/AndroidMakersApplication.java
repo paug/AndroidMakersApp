e51a9d5716dd9f5adaeeb157442384cc73e6e099
benju69 <begonin@gmail.com>
2018-04-01 16:12:41 +0200

    Move emoji compat code in Application class

--- app/src/main/java/fr/paug/androidmakers/AndroidMakersApplication.java
+++ app/src/main/java/fr/paug/androidmakers/AndroidMakersApplication.java
@@ -1,6 +1,11 @@
 package fr.paug.androidmakers;
 
 import android.app.Application;
+import android.support.annotation.Nullable;
+import android.support.text.emoji.EmojiCompat;
+import android.support.text.emoji.FontRequestEmojiCompatConfig;
+import android.support.v4.provider.FontRequest;
+import android.util.Log;
 
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -8,11 +13,41 @@ import fr.paug.androidmakers.util.SessionSelector;
 
 public class AndroidMakersApplication extends Application {
 
+    public static final String TAG = "AndroidMakersApplication";
+
     @Override
     public void onCreate() {
         super.onCreate();
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         SessionSelector.getInstance().init(this);
+
+        // Use a downloadable font for EmojiCompat
+        setupEmojiCompat();
+    }
+
+    private void setupEmojiCompat() {
+        final FontRequest fontRequest = new FontRequest(
+                "com.google.android.gms.fonts",
+                "com.google.android.gms",
+                "Noto Color Emoji Compat",
+                R.array.com_google_android_gms_fonts_certs);
+        final EmojiCompat.Config config = new FontRequestEmojiCompatConfig(getApplicationContext(), fontRequest)
+                .setReplaceAll(true)
+                .registerInitCallback(new EmojiCompat.InitCallback() {
+                    @Override
+                    public void onInitialized() {
+                        if (BuildConfig.DEBUG) {
+                            Log.i(TAG, "EmojiCompat initialized");
+                        }
+                    }
+
+                    @Override
+                    public void onFailed(@Nullable Throwable throwable) {
+                        Log.e(TAG, "EmojiCompat initialization failed", throwable);
+                    }
+                });
+
+        EmojiCompat.init(config);
     }
 
 }
