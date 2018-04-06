2d7f8b781df43d5722c52cce90865d2c9c2c0235
benju69 <begonin@gmail.com>
2018-04-06 23:33:20 +0200

    🐛 Fix possible crash in VenueFragment when data is not loaded

--- app/src/main/java/fr/paug/androidmakers/model/Venue.java
+++ app/src/main/java/fr/paug/androidmakers/model/Venue.java
@@ -6,12 +6,15 @@ package fr.paug.androidmakers.model;
  */
 public class Venue {
 
-    public String address;
-    public String coordinates;
-    public String description;
-    public String descriptionFr;
-    public String imageUrl;
-    public String name;
+    public String address = "";
+    public String coordinates = "";
+    public String description = "";
+    public String descriptionFr = "";
+    public String imageUrl = "";
+    public String name = "No Venue";
+
+    public Venue() {
+    }
 
     public Venue(String address, String coordinates, String description, String descriptionFr, String imageUrl, String name) {
         this.address = address;
