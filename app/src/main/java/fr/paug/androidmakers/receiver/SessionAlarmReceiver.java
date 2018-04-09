276831aff028de845c02ec039544d7a67dca9fe6
Benjamin GONIN <benjamin.gonin@seloger.com>
2018-04-09 13:40:22 +0200

    Refactors

--- app/src/main/java/fr/paug/androidmakers/receiver/SessionAlarmReceiver.java
+++ app/src/main/java/fr/paug/androidmakers/receiver/SessionAlarmReceiver.java
@@ -3,10 +3,11 @@ package fr.paug.androidmakers.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
+import android.support.v4.content.ContextCompat;
 
 import fr.paug.androidmakers.service.SessionAlarmService;
 
+// On boot, reschedule all favorites sessions
 public class SessionAlarmReceiver extends BroadcastReceiver {
 
     @Override
@@ -14,11 +15,7 @@ public class SessionAlarmReceiver extends BroadcastReceiver {
         Intent scheduleIntent = new Intent(
                 SessionAlarmService.ACTION_SCHEDULE_ALL_STARRED_BLOCKS,
                 null, context, SessionAlarmService.class);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            context.startForegroundService(scheduleIntent);
-        } else {
-            context.startService(scheduleIntent);
-        }
+        ContextCompat.startForegroundService(context, scheduleIntent);
     }
 
 }
