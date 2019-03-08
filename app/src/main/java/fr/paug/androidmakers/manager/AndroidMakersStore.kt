04bc338b906896d347067dd4aa6ba9c6c7e2b9bc
Benjamin GONIN <benjamin.gonin@seloger.com>
2019-03-08 14:28:19 +0100

    WIP Add Partners from firestore

--- app/src/main/java/fr/paug/androidmakers/manager/AndroidMakersStore.kt
+++ app/src/main/java/fr/paug/androidmakers/manager/AndroidMakersStore.kt
@@ -1,11 +1,13 @@
 package fr.paug.androidmakers.manager
 
+import android.util.Log
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
+import fr.paug.androidmakers.model.PartnerCollection
 
 class AndroidMakersStore {
 
-    fun getFireStore() : CollectionReference {
+    fun getFireStore(): CollectionReference {
         val firestore = FirebaseFirestore.getInstance()
         return firestore.collection("")
     }
@@ -13,6 +15,24 @@ class AndroidMakersStore {
     fun getSpeakers(): CollectionReference {
         val firestore = FirebaseFirestore.getInstance()
         return firestore.collection("speakers")
+    }
+
+    fun getPartners() : List<PartnerCollection> {
+        val partnersList = mutableListOf<PartnerCollection>()
+        val firestore = FirebaseFirestore.getInstance()
+        firestore.collection("partners")
+                .get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+                        Log.d("Firestore", document.id + " => " + document.data)
+                        val partnerCollection = document.toObject(PartnerCollection::class.java)
+                        partnersList.add(partnerCollection)
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w("Firestore", "Error getting documents.", exception)
+                }
+        return partnersList
     }
 
 }
