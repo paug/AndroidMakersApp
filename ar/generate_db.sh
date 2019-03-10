6a5b2dbf189ff143096dab77063c2d98aa7d8434
Martin Bonnin <martin@mbonnin.net>
2019-03-10 15:01:11 +0100

    add a picture of the beffroi (without a poster)

--- ar/generate_db.sh
+++ ar/generate_db.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/home/martin/android/arcore-android-sdk-1.8.0/tools/arcoreimg/linux/arcoreimg build-db --input_image_list_path=images.txt --output_db_path=../app/src/main/assets/images.imgdb
+/Users/martin/dev/arcore-android-sdk-1.7.0/tools/arcoreimg/macos/arcoreimg build-db --input_image_list_path=images.txt --output_db_path=../app/src/main/assets/images.imgdb
