bb94df777cd30dd8a3c5869ce6fa7c3db11c2fb9
Benjamin GONIN <benjamin.gonin@seloger.com>
2019-04-03 14:59:47 +0200

    Use a schedule slot object

---
+++ app/src/main/java/fr/paug/androidmakers/model/ScheduleSlotKt.kt
@@ -0,0 +1,8 @@
+package fr.paug.androidmakers.model
+
+data class ScheduleSlotKt(
+        val endDate: String = "",
+        val sessionId: String = "",
+        val roomId: String = "",
+        val startDate: String = ""
+)
---
+++ app/src/main/java/fr/paug/androidmakers/model/ScheduleSlotList.kt
@@ -0,0 +1,5 @@
+package fr.paug.androidmakers.model
+
+data class ScheduleSlotList(
+        var all: ArrayList<ScheduleSlotKt> = arrayListOf()
+)
