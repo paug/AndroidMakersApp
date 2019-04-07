f5b4b556571b32b73df126494559f8c2f88080d2
benju69 <begonin@gmail.com>
2019-04-07 22:48:06 +0200

    Restore bookmarks in list, restore filters. Clean code

--- app/src/main/java/fr/paug/androidmakers/ui/util/SessionFilter.java
+++ app/src/main/java/fr/paug/androidmakers/ui/util/SessionFilter.java
@@ -1,14 +1,12 @@
 package fr.paug.androidmakers.ui.util;
 
 public class SessionFilter {
+
     public enum FilterType {
         BOOKMARK,
         LANGUAGE,
         ROOM
     }
-
-    public static final String LANGUAGE_FRENCH = "fr";
-    public static final String LANGUAGE_ENGLISH = "en";
 
     public final Object value;
     public final FilterType type;
