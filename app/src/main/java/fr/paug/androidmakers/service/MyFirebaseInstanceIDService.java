db5303efd5ef4d8961845949dd6d21198b384893
Martin Bonnin <martin@mbonnin.net>
2020-04-11 10:34:51 +0200

    bump dependencies versions, remove youtube and ARCore

--- app/src/main/java/fr/paug/androidmakers/service/MyFirebaseInstanceIDService.java
+++ app/src/main/java/fr/paug/androidmakers/service/MyFirebaseInstanceIDService.java
@@ -18,31 +18,23 @@ package fr.paug.androidmakers.service;
 
 import android.util.Log;
 
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
+import androidx.annotation.NonNull;
 
-public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseInstanceIDService extends FirebaseMessagingService {
 
     private static final String TAG = "MyFirebaseIIDService";
 
-    /**
-     * Called if InstanceID token is updated. This may occur if the security of
-     * the previous token had been compromised. Note that this is called when the InstanceID token
-     * is initially generated so this is where you would retrieve the token.
-     */
-    // [START refresh_token]
     @Override
-    public void onTokenRefresh() {
-        // Get updated InstanceID token.
-        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        Log.d(TAG, "Refreshed token: " + refreshedToken);
+    public void onNewToken(@NonNull String token) {
+        Log.d(TAG, "Refreshed token: $token");
 
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
         // Instance ID token to your app server.
-        sendRegistrationToServer(refreshedToken);
+        sendRegistrationToServer(token);
     }
-    // [END refresh_token]
 
     /**
      * Persist token to third-party servers.
