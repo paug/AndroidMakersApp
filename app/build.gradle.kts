1f968e4d7c1662523cf68930f9e63468bb5cf154
Martin Bonnin <martin@mbonnin.net>
2020-04-17 19:30:51 +0200

    no slido for coffee break

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 val versionMajor = 1
 val versionMinor = 2
-val versionPatch = 8
+val versionPatch = 9
 
 fun checkGoogleServices() {
     val target = project.file("google-services.json")
