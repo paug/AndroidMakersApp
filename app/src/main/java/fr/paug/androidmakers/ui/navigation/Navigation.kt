0aa5bdf721d1c04aaf7ba038c2c6563c4ff37412
BoD <BoD@JRAF.org>
2022-04-10 16:14:14 +0200

    Add composed based navigation

---
+++ app/src/main/java/fr/paug/androidmakers/ui/navigation/Navigation.kt
@@ -0,0 +1,15 @@
+package fr.paug.androidmakers.ui.navigation
+
+enum class MainNavigationRoute {
+    /**
+     * AVA stands for Agenda/Venue/About
+     */
+    AVA,
+    SESSION_DETAIL,
+}
+
+enum class AVANavigationRoute {
+    AGENDA,
+    VENUE,
+    ABOUT,
+}
