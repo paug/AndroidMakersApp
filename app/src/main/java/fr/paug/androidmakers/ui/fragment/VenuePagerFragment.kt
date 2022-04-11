4078a3357da1dcdd21ed0bb2ba117494fda212c0
Martin Bonnin <martin@mbonnin.net>
2022-04-11 20:57:37 +0200

    add accompanist

--- app/src/main/java/fr/paug/androidmakers/ui/fragment/VenuePagerFragment.kt
+++ app/src/main/java/fr/paug/androidmakers/ui/fragment/VenuePagerFragment.kt
@@ -4,24 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
-import androidx.viewpager.widget.ViewPager
-import fr.paug.androidmakers.R
-import fr.paug.androidmakers.ui.adapter.VenuePagerAdapter
+import fr.paug.androidmakers.ui.components.VenuePager
+import fr.paug.androidmakers.ui.theme.AndroidMakersTheme
 
 class VenuePagerFragment : Fragment() {
 
-    private var viewPager: ViewPager? = null
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.venue_pager, container, false)
-        viewPager = view.findViewById(R.id.venue_viewpager)
+        val view = ComposeView(requireContext())
+        view.setContent {
+            AndroidMakersTheme {
+                VenuePager()
+            }
+        }
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewPager!!.adapter = VenuePagerAdapter(requireContext(), childFragmentManager)
     }
-
 }
