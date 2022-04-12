b93d760ffca98692e60f5818e347c231acc80fa4
BoD <BoD@JRAF.org>
2022-04-12 16:06:11 +0200

    AboutLayout: data and actions

--- app/src/main/java/fr/paug/androidmakers/ui/activity/Main2Activity.kt
+++ app/src/main/java/fr/paug/androidmakers/ui/activity/Main2Activity.kt
@@ -1,14 +1,85 @@
 package fr.paug.androidmakers.ui.activity
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import fr.paug.androidmakers.R
+import fr.paug.androidmakers.util.CustomTabUtil
 
 class Main2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            MainLayout()
+            MainLayout(aboutActions = AboutActions(
+                onFaqClick = ::onFaqClick,
+                onCodeOfConductClick = ::onCodeOfConductClick,
+                onTwitterHashtagClick = ::onTwitterHashtagClick,
+                onTwitterLogoClick = ::onTwitterLogoClick,
+                onYouTubeLogoClick = ::onYouTubeLogoClick,
+                onSponsorClick = ::onSponsorClick
+            ))
         }
     }
+
+    private fun onFaqClick() {
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://androidmakers.fr/faq")))
+    }
+
+    private fun onCodeOfConductClick() {
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://androidmakers.fr/coc")))
+    }
+
+    private fun onTwitterHashtagClick() {
+        var twitterIntent: Intent
+        try {
+            // get the Twitter app if possible
+            packageManager?.getPackageInfo("com.twitter.android", 0)
+            twitterIntent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("twitter://search?query=%23" + getString(R.string.twitter_hashtag_for_query))
+            )
+            twitterIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        } catch (e: Exception) {
+            // no Twitter app, revert to browser
+            twitterIntent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://twitter.com/search?q=%23" + getString(R.string.twitter_hashtag_for_query))
+            )
+        }
+
+        startActivity(twitterIntent)
+    }
+
+    private fun onTwitterLogoClick() {
+        var twitterIntent: Intent
+        try {
+            // get the Twitter app if possible
+            packageManager?.getPackageInfo("com.twitter.android", 0)
+            twitterIntent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("twitter://user?screen_name=" + getString(R.string.twitter_user_name))
+            )
+            twitterIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        } catch (e: Exception) {
+            // no Twitter app, revert to browser
+            twitterIntent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://twitter.com/" + getString(R.string.twitter_user_name))
+            )
+        }
+
+        startActivity(twitterIntent)
+    }
+
+    private fun onYouTubeLogoClick() {
+        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.youtube_channel))))
+    }
+
+    private fun onSponsorClick(url: String) {
+        CustomTabUtil.openChromeTab(this, url)
+    }
+
 }
