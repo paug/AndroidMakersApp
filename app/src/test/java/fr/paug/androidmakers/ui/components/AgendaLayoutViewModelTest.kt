926e44c5ffe1a1fbfa021e75fdd33b9b44b462b3
Ahmed I. Khalil <ahmedibrahimkhali@gmail.com>
2022-05-04 22:49:05 +0200

    Add tests for AgendaLayoutViewModelTest

---
+++ app/src/test/java/fr/paug/androidmakers/ui/components/AgendaLayoutViewModelTest.kt
@@ -0,0 +1,72 @@
+package fr.paug.androidmakers.ui.components
+
+import fr.androidmakers.store.model.Room
+import fr.paug.androidmakers.fixtures.FakeAndroidMakersStore
+import fr.paug.androidmakers.util.SessionFilter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.lang.RuntimeException
+
+class AgendaLayoutViewModelTest {
+
+  private val fakeStore = FakeAndroidMakersStore()
+
+  private val testSubject = AgendaLayoutViewModel(
+    store = fakeStore,
+    scope = { CoroutineScope(Dispatchers.Unconfined) }
+  )
+
+  @Test
+  fun `initial state should be correct`() = runBlocking {
+    assertEquals(
+      AgendaLayoutState(
+        rooms = emptyList(),
+        sessionFilters = emptyList()
+      ),
+      testSubject.state.first()
+    )
+  }
+
+  @Test
+  fun `given non empty session filters state should be correct`() = runBlocking {
+    testSubject.onFiltersChanged(listOf(SessionFilter(SessionFilter.FilterType.BOOKMARK, "bookmark")))
+
+    assertEquals(
+      AgendaLayoutState(
+        rooms = emptyList(),
+        sessionFilters = listOf(SessionFilter(SessionFilter.FilterType.BOOKMARK, "bookmark"))
+      ),
+      testSubject.state.first()
+    )
+  }
+
+  @Test
+  fun `given failure in retrieving rooms state should have empty rooms`() = runBlocking {
+    fakeStore.roomsMutableFlow.value = Result.failure(RuntimeException("exception"))
+
+    assertEquals(
+      AgendaLayoutState(
+        rooms = emptyList(),
+        sessionFilters = emptyList()
+      ),
+      testSubject.state.first()
+    )
+  }
+
+  @Test
+  fun `given rooms are retrieved state should be correct`() = runBlocking {
+    fakeStore.roomsMutableFlow.value = Result.success(listOf(Room(id = "id", name = "name")))
+
+    assertEquals(
+      AgendaLayoutState(
+        rooms = listOf(Room(id = "id", name = "name")),
+        sessionFilters = emptyList()
+      ),
+      testSubject.state.first()
+    )
+  }
+}
