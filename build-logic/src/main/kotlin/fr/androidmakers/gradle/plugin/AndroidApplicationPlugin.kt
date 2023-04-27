24484459c260d1482d7b350fa71111324cc948d1
Martin Bonnin <martin@mbonnin.net>
2023-04-27 23:17:41 +0200

    version 1456 (#173)

--- build-logic/src/main/kotlin/fr/androidmakers/gradle/plugin/AndroidApplicationPlugin.kt
+++ build-logic/src/main/kotlin/fr/androidmakers/gradle/plugin/AndroidApplicationPlugin.kt
@@ -26,7 +26,7 @@ class AndroidApplicationPlugin : Plugin<Project> {
         namespace = "fr.paug.androidmakers"
         minSdk = 21
         targetSdk = 33
-        versionCode = 1455
+        versionCode = 1456
         versionName = versionCode.toString()
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
       }
