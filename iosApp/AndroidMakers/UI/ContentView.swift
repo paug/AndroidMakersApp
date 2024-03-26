bfa9c2b007ba53acc5a3060a55edc7cb789f98aa
Antoine Robiez <antoine.robiez@icloud.com>
2024-03-26 09:38:33 +0100

    Big clean on the iOS project (#213)
    * Big clean on iOS project
    
    ---------
    
    Co-authored-by: Antoine Robiez <antoine.robiez@icloud.com>

--- iosApp/AndroidMakers/UI/ContentView.swift
+++ iosApp/AndroidMakers/UI/ContentView.swift
@@ -6,21 +6,10 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-    @State private var selection = 0
-
     var body: some View {
         MainView()
     }
 }
-
-#if DEBUG
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        injectMockModel()
-        return ContentView()
-    }
-}
-#endif
 
 struct MainView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
