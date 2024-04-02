8bf777fa900ae0ed712e2ae573f737a4fccbc08c
Benoit 'BoD' Lubek <BoD@JRAF.org>
2024-04-02 13:04:56 +0200

    Wear app: Scroll to next session (#234)

--- wearApp/src/main/java/fr/paug/androidmakers/wear/ui/session/UISession.kt
+++ wearApp/src/main/java/fr/paug/androidmakers/wear/ui/session/UISession.kt
@@ -3,12 +3,27 @@ package fr.paug.androidmakers.wear.ui.session
 import fr.androidmakers.domain.model.Room
 import fr.androidmakers.domain.model.Session
 import fr.androidmakers.domain.model.Speaker
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 
 data class UISession(
-    val session: Session,
-    val speakers: List<Speaker>,
-    val room: Room,
-    val isBookmarked: Boolean,
+  val session: Session,
+  val speakers: List<Speaker>,
+  val room: Room,
+  val isBookmarked: Boolean,
 ) {
   val formattedDuration: String = session.duration.inWholeMinutes.toString() + " min"
+
+  val isOver: Boolean
+    get() {
+      val now = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+      return session.endsAt < now
+    }
+
+  val isOngoing: Boolean
+    get() {
+      val now = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+      return session.startsAt <= now && now <= session.endsAt
+    }
 }
