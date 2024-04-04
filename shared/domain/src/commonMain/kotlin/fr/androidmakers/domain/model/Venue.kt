2f184832f2c4e6d7b20215f2217fb86683006065
Martin Bonnin <martin@mbonnin.net>
2024-04-04 23:39:28 +0200

    Load the floor plan from the network

--- shared/domain/src/commonMain/kotlin/fr/androidmakers/domain/model/Venue.kt
+++ shared/domain/src/commonMain/kotlin/fr/androidmakers/domain/model/Venue.kt
@@ -6,5 +6,6 @@ data class Venue(
     var description: String = "",
     var descriptionFr: String = "",
     var imageUrl: String = "",
+    val floorPlanUrl: String = "",
     var name: String = "No Venue"
 )
--- shared/ui/src/commonMain/kotlin/com/androidmakers/ui/venue/FloorPlan.kt
+++ shared/ui/src/commonMain/kotlin/com/androidmakers/ui/venue/FloorPlan.kt
@@ -2,18 +2,25 @@ package com.androidmakers.ui.venue
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import com.seiko.imageloader.rememberImagePainter
 import dev.icerock.moko.resources.compose.painterResource
 import fr.paug.androidmakers.ui.MR
 
 @Composable
-fun FloorPlan() {
+fun FloorPlan(floorPlanUrl: String) {
   Box(modifier = Modifier.verticalScroll(rememberScrollState())) {
+    val painter = rememberImagePainter(floorPlanUrl)
+
     Image(
-        painter = painterResource(MR.images.beffroi),
+        painter = painter,
         contentDescription = "floor plan"
     )
   }
