d9d39664a62582885af52375b1f73b17008a9e98
Adrien <n0tnull@users.noreply.github.com>
2024-04-11 18:19:47 +0200

    Fix a weird scroll when opening the floor plan screen (#256)

--- shared/ui/src/commonMain/kotlin/com/androidmakers/ui/venue/FloorPlan.kt
+++ shared/ui/src/commonMain/kotlin/com/androidmakers/ui/venue/FloorPlan.kt
@@ -2,21 +2,22 @@ package com.androidmakers.ui.venue
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.dp
 import com.seiko.imageloader.rememberImagePainter
-import dev.icerock.moko.resources.compose.painterResource
-import fr.paug.androidmakers.ui.MR
 
 @Composable
 fun FloorPlan(floorPlanUrl: String) {
-  Box(modifier = Modifier.verticalScroll(rememberScrollState())) {
+  Box(
+      modifier = Modifier
+          .fillMaxWidth()
+          .fillMaxHeight()
+          .verticalScroll(rememberScrollState())
+  ) {
     val painter = rememberImagePainter(floorPlanUrl)
 
     Image(
