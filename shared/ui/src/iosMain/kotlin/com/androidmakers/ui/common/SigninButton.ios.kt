b15b67a904f2041457aa193471ab642c00d4dc5b
Antoine Robiez <antoine.robiez@icloud.com>
2024-04-13 13:52:42 +0200

    Disabled iOS Sign In (#258)
    Co-authored-by: Antoine Robiez <antoine.robiez@worldline.com>

--- shared/ui/src/iosMain/kotlin/com/androidmakers/ui/common/SigninButton.ios.kt
+++ shared/ui/src/iosMain/kotlin/com/androidmakers/ui/common/SigninButton.ios.kt
@@ -3,13 +3,9 @@ package com.androidmakers.ui.common
 import androidx.compose.runtime.Composable
 import fr.androidmakers.domain.model.User
 
-class SigninCallbacks(
-  val signin: () -> Unit,
-  val signout: () -> Unit,
-)
-
 @Composable
-expect fun SigninButton(
+actual fun SigninButton(
   user: User?,
-  callbacks: SigninCallbacks,
-)
+  callbacks: SigninCallbacks
+) {
+}
