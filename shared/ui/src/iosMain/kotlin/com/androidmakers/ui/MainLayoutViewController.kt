58d00111f78222b74e3ef2d6b63a544cde8d0ff8
Antoine Robiez <antoine.robiez@icloud.com>
2024-04-13 13:52:42 +0200

    Disabled iOS Sign In (#258)
    Co-authored-by: Antoine Robiez <antoine.robiez@worldline.com>

--- shared/ui/src/iosMain/kotlin/com/androidmakers/ui/MainLayoutViewController.kt
+++ shared/ui/src/iosMain/kotlin/com/androidmakers/ui/MainLayoutViewController.kt
@@ -1,6 +1,7 @@
 package com.androidmakers.ui
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.androidmakers.ui.common.SigninCallbacks
 import com.androidmakers.ui.theme.AndroidMakersTheme
 import platform.UIKit.UIViewController
 
@@ -9,9 +10,12 @@ fun MainLayoutViewController(): UIViewController =
     ComposeUIViewController {
       AndroidMakersTheme {
         MainLayout(
-            user = null,
             versionName = "1.0.0",
             versionCode = "1.0",
+          signinCallbacks = SigninCallbacks(
+            signin = {},
+            signout = {}
+          )
         )
       }
     }
