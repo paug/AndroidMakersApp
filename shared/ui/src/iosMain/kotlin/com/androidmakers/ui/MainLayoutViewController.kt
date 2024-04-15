0aa67831717025c56e18b0ee0bdcf4b022f27c59
Antoine Robiez <antoine.robiez@icloud.com>
2024-04-15 16:36:33 +0200

    Version 4.1 / 1465 (#261)

--- shared/ui/src/iosMain/kotlin/com/androidmakers/ui/MainLayoutViewController.kt
+++ shared/ui/src/iosMain/kotlin/com/androidmakers/ui/MainLayoutViewController.kt
@@ -3,15 +3,20 @@ package com.androidmakers.ui
 import androidx.compose.ui.window.ComposeUIViewController
 import com.androidmakers.ui.common.SigninCallbacks
 import com.androidmakers.ui.theme.AndroidMakersTheme
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.NSBundle
 import platform.UIKit.UIViewController
 
 
 fun MainLayoutViewController(): UIViewController =
     ComposeUIViewController {
+      val versionName = NSBundle.mainBundle.objectForInfoDictionaryKey("CFBundleShortVersionString")
+      val versionCode = NSBundle.mainBundle.objectForInfoDictionaryKey("CFBundleVersion")
+
       AndroidMakersTheme {
         MainLayout(
-            versionName = "1.0.0",
-            versionCode = "1.0",
+            versionName = versionName.toString(),
+            versionCode = versionCode.toString(),
           signinCallbacks = SigninCallbacks(
             signin = {},
             signout = {}
