7990e89a820bb169f88c1c372429738b3686e862
Martin Bonnin <martin@mbonnin.net>
2024-04-19 18:45:00 +0200

    Call openUrl with a completionCallback, seems to fix a freeze (#291)

--- shared/domain/src/iosMain/kotlin/fr/androidmakers/domain/utils/UrlOpener.ios.kt
+++ shared/domain/src/iosMain/kotlin/fr/androidmakers/domain/utils/UrlOpener.ios.kt
@@ -2,13 +2,24 @@ package fr.androidmakers.domain.utils
 
 import platform.Foundation.NSURL
 import platform.UIKit.UIApplication
+import platform.darwin.dispatch_async
+import platform.darwin.dispatch_get_main_queue
 
 actual class UrlOpener {
   actual fun openUrl(url: String): Boolean {
     val urlObj = NSURL(string = url)
     return if (UIApplication.sharedApplication.canOpenURL(urlObj)) {
-      UIApplication.sharedApplication.openURL(urlObj)
+      val application = UIApplication.sharedApplication
+
+      application.openURL(url = urlObj, options = emptyMap<Any?, Any>()) {
+        if (!it) {
+          println("Could not open $url")
+        }
+      }
       true
-    } else { false }
+    } else {
+      false
+    }
   }
 }
+
