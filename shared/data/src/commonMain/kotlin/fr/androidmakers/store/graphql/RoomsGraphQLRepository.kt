86e569e84475dc2b71d157e6ef3d870a65bdf4bd
Martin Bonnin <martin@mbonnin.net>
2024-04-19 17:22:06 +0200

    Make the app work offline (#287)
    * make the app work offline
    
    * remove watch
    
    * merge bookmarks on signin

--- shared/data/src/commonMain/kotlin/fr/androidmakers/store/graphql/RoomsGraphQLRepository.kt
+++ shared/data/src/commonMain/kotlin/fr/androidmakers/store/graphql/RoomsGraphQLRepository.kt
@@ -1,9 +1,6 @@
 package fr.androidmakers.store.graphql
 
 import com.apollographql.apollo3.ApolloClient
-import com.apollographql.apollo3.cache.normalized.FetchPolicy
-import com.apollographql.apollo3.cache.normalized.fetchPolicy
-import com.apollographql.apollo3.cache.normalized.watch
 import fr.androidmakers.domain.model.Room
 import fr.androidmakers.domain.repo.RoomsRepository
 import kotlinx.coroutines.flow.Flow
@@ -18,11 +15,7 @@ class RoomsGraphQLRepository(private val apolloClient: ApolloClient): RoomsRepos
 
   override fun getRooms(): Flow<Result<List<Room>>> {
     return apolloClient.query(GetRoomsQuery())
-        .fetchPolicy(FetchPolicy.NetworkFirst)
-        .watch()
-        .map {
-          it.dataAssertNoErrors.rooms.map { it.roomDetails.toRoom() }
-        }
-        .toResultFlow()
+        .cacheAndNetwork()
+        .map { it.map { it.rooms.map { it.roomDetails.toRoom() } } }
   }
 }
--- shared/ui/src/commonMain/kotlin/com/androidmakers/ui/common/navigation/UserViewModel.kt
+++ shared/ui/src/commonMain/kotlin/com/androidmakers/ui/common/navigation/UserViewModel.kt
@@ -1,11 +1,13 @@
 package com.androidmakers.ui.common.navigation
 
+import fr.androidmakers.domain.interactor.SyncBookmarksUseCase
 import fr.androidmakers.domain.repo.UserRepository
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 class UserData: KoinComponent {
   val userRepository: UserRepository by inject()
+  val syncBookmarksUseCase: SyncBookmarksUseCase by inject()
 }
 
 
