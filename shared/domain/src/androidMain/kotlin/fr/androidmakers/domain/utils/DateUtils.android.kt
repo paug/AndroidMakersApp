c497172501758c6759cfb0ed06a07631056109cb
Christophe Beyls <cbeyls@users.noreply.github.com>
2024-05-27 14:48:55 +0200

    Optimize Android date and time formatting (#315)
    * use DateTimeFormatter to format dates on Android and cache it for the current Locale.
    
    * turn the KeyValueCache class into a memoize() extension function
    
    * remove unnecessary parenthesis
    
    * avoid double calls to Locale.getDefault()

--- shared/domain/src/androidMain/kotlin/fr/androidmakers/domain/utils/DateUtils.android.kt
+++ shared/domain/src/androidMain/kotlin/fr/androidmakers/domain/utils/DateUtils.android.kt
@@ -1,52 +1,61 @@
+@file:SuppressLint("NewApi")
+
 package fr.androidmakers.domain.utils
 
-import android.text.format.DateUtils
+import android.annotation.SuppressLint
 import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.atStartOfDayIn
-import kotlinx.datetime.toInstant
-import kotlinx.datetime.toJavaLocalDateTime
-import java.text.DateFormat.MEDIUM
-import java.text.DateFormat.SHORT
-import java.text.SimpleDateFormat
+import kotlinx.datetime.toJavaLocalDate
+import kotlinx.datetime.toJavaLocalTime
+import kotlinx.datetime.toLocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
-import java.util.Date
 import java.util.Locale
-import java.util.TimeZone
+import java.util.concurrent.atomic.AtomicReference
+
+/**
+ * Memoize the result of the function call until the key changes.
+ */
+private fun <K, V> ((K) -> V).memoize() : (K) -> V {
+  val cache = AtomicReference<Pair<K, V>?>(null)
+  return { key: K ->
+    val currentPair = cache.get()
+    if (currentPair != null && currentPair.first == key) {
+      currentPair.second
+    } else {
+      val newPair = key to this(key)
+      // Only update the cache if it didn't change in the meantime
+      cache.compareAndSet(currentPair, newPair)
+      newPair.second
+    }
+  }
+}
+
+private val shortTimeFormatterProvider = { locale: Locale ->
+  DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT).withLocale(locale)
+}.memoize()
+
+private val mediumDateFormatterProvider = { locale: Locale ->
+  DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM).withLocale(locale)
+}.memoize()
 
 actual fun LocalDateTime.formatShortTime(): String {
-  return this.toInstant(kotlinx.datetime.TimeZone.currentSystemDefault()).formatShortTime()
+  val locale = Locale.getDefault()
+  val shortTimeFormatter = shortTimeFormatterProvider(locale)
+  return time.toJavaLocalTime().format(shortTimeFormatter).lowercase(locale)
 }
 
 actual fun Instant.formatShortTime(): String {
-  val dateFormat = SimpleDateFormat.getTimeInstance(SHORT)
-
   // Display the time in the local time as everyone will be on site so it makes
   // planning in advance easier
-  val tz = TimeZone.getTimeZone("Europe/Paris")
-  if (tz != null) {
-    dateFormat.timeZone = tz
-  }
-
-  val date = Date(this.toEpochMilliseconds())
-  return dateFormat.format(date).lowercase(Locale.getDefault())
+  return toLocalDateTime(eventTimeZone).formatShortTime()
 }
 
 actual fun LocalDate.formatMediumDate(): String {
-  val dateFormat = SimpleDateFormat.getDateInstance(MEDIUM)
-
-  val tz = TimeZone.getTimeZone("Europe/Paris")
-  if (tz != null) {
-    dateFormat.timeZone = tz
-  }
-
-  val date = Date(
-      this.atStartOfDayIn(kotlinx.datetime.TimeZone.currentSystemDefault())
-      .toEpochMilliseconds()
-  )
-  return dateFormat.format(date).lowercase(Locale.getDefault())
+  val locale = Locale.getDefault()
+  val mediumDateFormatter = mediumDateFormatterProvider(locale)
+  return toJavaLocalDate().format(mediumDateFormatter).lowercase(locale)
 }
 
 actual fun formatTimeInterval(startDate: LocalDateTime, endDate: LocalDateTime): String {
