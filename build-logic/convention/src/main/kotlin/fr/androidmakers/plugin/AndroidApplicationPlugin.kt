733441203a1921c807bb2edf41a151d586827702
Christophe Beyls <software@digitalia.be>
2025-02-18 19:47:40 +0100

    chore: set JVM Target 17 for all projects

--- build-logic/convention/src/main/kotlin/fr/androidmakers/plugin/AndroidApplicationPlugin.kt
+++ build-logic/convention/src/main/kotlin/fr/androidmakers/plugin/AndroidApplicationPlugin.kt
@@ -5,8 +5,10 @@ import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.assign
 import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 class AndroidApplicationPlugin : Plugin<Project> {
@@ -48,8 +50,8 @@ internal fun Project.configureKotlinAndroid(
 
 private fun Project.configureKotlin() {
   tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions {
-      jvmTarget = JavaVersion.VERSION_17.toString()
+    compilerOptions {
+      jvmTarget = JvmTarget.JVM_17
     }
   }
 }
