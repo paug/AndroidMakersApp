245210f53a3a15987db4315523c3c2a73f20f0db
Christophe Beyls <software@digitalia.be>
2025-03-02 22:27:40 +0100

    chore: remove unnecessary WindowInsets override in AVALayout

--- shared/ui/src/commonMain/kotlin/com/androidmakers/ui/common/navigation/AVALayout.kt
+++ shared/ui/src/commonMain/kotlin/com/androidmakers/ui/common/navigation/AVALayout.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -88,7 +87,6 @@ fun AVALayout(
 
   Scaffold(
       modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-      contentWindowInsets = WindowInsets(0, 0, 0, 0),
       topBar = {
         TopAppBar(
             scrollBehavior = scrollBehavior,
