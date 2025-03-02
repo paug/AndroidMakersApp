abf630a45756c328db7b6fff3b3f2128f8c3776e
Christophe Beyls <software@digitalia.be>
2025-03-02 13:35:30 +0100

    chore: remove unused compose resources (will be used in the future to replace moko)

--- shared/ui/build.gradle.kts
+++ shared/ui/build.gradle.kts
@@ -26,7 +26,6 @@ kotlin {
       implementation(compose.foundation)
       implementation(compose.material3)
       implementation(compose.ui)
-      implementation(compose.components.resources)
       implementation(compose.materialIconsExtended)
       implementation(compose.components.uiToolingPreview)
       implementation(libs.coil.compose)
