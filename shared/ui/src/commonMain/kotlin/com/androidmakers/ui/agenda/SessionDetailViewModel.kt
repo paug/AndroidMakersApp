f736fa533ea817c6c43b8d3815d6c88514207cf5
Christophe Beyls <software@digitalia.be>
2025-03-06 20:19:24 +0100

    refactor: load sessiondetails in a single stream and cache the results

--- shared/ui/src/commonMain/kotlin/com/androidmakers/ui/agenda/SessionDetailViewModel.kt
+++ shared/ui/src/commonMain/kotlin/com/androidmakers/ui/agenda/SessionDetailViewModel.kt
@@ -9,22 +9,18 @@ import fr.androidmakers.domain.interactor.ApplyForAppClinicUseCase
 import fr.androidmakers.domain.interactor.OpenLinkUseCase
 import fr.androidmakers.domain.interactor.SetSessionBookmarkUseCase
 import fr.androidmakers.domain.interactor.ShareSessionUseCase
-import fr.androidmakers.domain.model.Room
-import fr.androidmakers.domain.model.Session
 import fr.androidmakers.domain.model.SocialsItem
-import fr.androidmakers.domain.model.Speaker
 import fr.androidmakers.domain.repo.BookmarksRepository
 import fr.androidmakers.domain.repo.RoomsRepository
 import fr.androidmakers.domain.repo.SessionsRepository
 import fr.androidmakers.domain.repo.SpeakersRepository
-import fr.androidmakers.domain.utils.formatTimeInterval
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flatMapConcat
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.mapNotNull
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.transformLatest
 import kotlinx.coroutines.launch
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toInstant
@@ -42,72 +38,55 @@ class SessionDetailViewModel(
     private val applyForAppClinicUseCase: ApplyForAppClinicUseCase,
 ) : ViewModel() {
 
-  private val session: Flow<Result<Session>> = sessionsRepository.getSession(sessionId)
-  private val room: Flow<Result<Room>> = session.flatMapConcat{ result ->
-    result.fold(
-      onSuccess = { roomsRepository.getRoom(it.roomId) },
-      onFailure = { flowOf(Result.failure(it)) }
+  val sessionDetailState: StateFlow<Lce<SessionDetailState>> = sessionsRepository.getSession(sessionId)
+    .transformLatest { sessionResult ->
+      sessionResult
+        .onSuccess { session ->
+          combine(
+            roomsRepository.getRoom(session.roomId),
+            speakersRepository.getSpeakers(session.speakers),
+            bookmarksRepository.isBookmarked(sessionId)
+          ) { roomResult, speakersResult, isBookmarked ->
+            val room = roomResult.getOrElse { return@combine Lce.Error }
+            val speakers = speakersResult.getOrElse { return@combine Lce.Error }
+            Lce.Content(
+              SessionDetailState(
+                session = session,
+                room = room,
+                speakers = speakers,
+                startTimestamp = session.startsAt.toInstant(TimeZone.currentSystemDefault()),
+                endTimestamp = session.endsAt.toInstant(TimeZone.currentSystemDefault()),
+                isBookmarked = isBookmarked,
+              )
+            )
+          }.collect(this)
+        }
+        .onFailure {
+          emit(Lce.Error)
+        }
+    }.stateIn(
+      scope = viewModelScope,
+      started = SharingStarted.Eagerly,
+      initialValue = Lce.Loading
     )
-  }
-
-  private val speakers: Flow<List<Speaker>> = session.mapNotNull { result ->
-    result.getOrNull()?.speakers?.let { speakerIds ->
-      speakersRepository.getSpeakers(speakerIds).first().getOrNull()
-    }
-  }
-
-  private val formattedDateAndRoom: Flow<String> =
-    combine(session, room) { sessionResult, roomResult ->
-      sessionResult.fold(
-        onSuccess = {
-          val sessionDate = formatTimeInterval(
-            it.startsAt,
-            it.endsAt
-          )
-          val roomName = roomResult.getOrNull()?.name
-          if (!roomName.isNullOrEmpty()) {
-            "$sessionDate, $roomName"
-          } else {
-            sessionDate
-          }
-        },
-        onFailure = { "" }
-      )
-    }
-
-  val sessionDetailState: Flow<Lce<SessionDetailState>> = combine(
-    session,
-    room,
-    bookmarksRepository.isBookmarked(sessionId),
-  ) { sessionResult, roomResult, isBookmarked ->
-
-    val session = sessionResult.getOrElse {
-      return@combine Lce.Error
-    }
-    val room = roomResult.getOrElse {
-      return@combine Lce.Error
-    }
-    Lce.Content(
-      SessionDetailState(
-        session = session,
-        room = room,
-        speakers = speakers.first(),
-        startTimestamp = session.startsAt.toInstant(TimeZone.currentSystemDefault()),
-        endTimestamp = session.endsAt.toInstant(TimeZone.currentSystemDefault()),
-        isBookmarked = isBookmarked,
-      )
-    )
-  }
 
   fun bookmark(bookmarked: Boolean) = viewModelScope.launch {
-    session.first().onSuccess {
-      setSessionBookmarkUseCase(it.id, bookmarked)
+    val lce = sessionDetailState.first { it !is Lce.Loading }
+    if (lce is Lce.Content) {
+      setSessionBookmarkUseCase(lce.content.session.id, bookmarked)
     }
   }
 
   fun shareSession(platformContext: PlatformContext) = viewModelScope.launch {
-    session.first().onSuccess {
-      shareSessionUseCase(platformContext, it, speakers.first(), formattedDateAndRoom.first())
+    val lce = sessionDetailState.first { it !is Lce.Loading }
+    if (lce is Lce.Content) {
+      val state = lce.content
+      shareSessionUseCase(
+        platformContext = platformContext,
+        session = state.session,
+        speakers = state.speakers,
+        formattedDateAndRoom = state.formattedDateAndRoom
+      )
     }
   }
 
