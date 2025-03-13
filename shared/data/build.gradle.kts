06c6e7e42d5c5c3facd0a30db2bb13595589025f
Martin Bonnin <martin@mbonnin.net>
2025-03-14 00:51:04 +0100

    remove unused code

--- shared/data/build.gradle.kts
+++ shared/data/build.gradle.kts
@@ -52,9 +52,6 @@ apollo {
     introspection {
       schemaFile.set(file("src/commonMain/graphql/schema.graphqls"))
       endpointUrl.set("https://androidmakers.fr/graphql")
-
-      // This header is not needed to fetch the schema but it's read by the Apollo IDE plugin which will inject it when executing queries
-      headers.set(mapOf("conference" to "androidmakers2025"))
     }
   }
 }
