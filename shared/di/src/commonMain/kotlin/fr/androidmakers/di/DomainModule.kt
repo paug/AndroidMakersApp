54f39e1e07f0244b832438e02385ca8a05988ea5
Christophe Beyls <software@digitalia.be>
2025-03-15 17:58:56 +0100

    refactor: use singleOf(), factoryOf() and viewModelOf() koin module definitions where applicable

--- shared/di/src/commonMain/kotlin/fr/androidmakers/di/DomainModule.kt
+++ shared/di/src/commonMain/kotlin/fr/androidmakers/di/DomainModule.kt
@@ -7,35 +7,36 @@ import fr.androidmakers.domain.interactor.GetConferenceVenueUseCase
 import fr.androidmakers.domain.interactor.GetFavoriteSessionsUseCase
 import fr.androidmakers.domain.interactor.GetPartnersUseCase
 import fr.androidmakers.domain.interactor.MergeBookmarksUseCase
+import fr.androidmakers.domain.interactor.OpenBlueskyAccountUseCase
 import fr.androidmakers.domain.interactor.OpenCocUseCase
 import fr.androidmakers.domain.interactor.OpenFaqUseCase
 import fr.androidmakers.domain.interactor.OpenLinkUseCase
 import fr.androidmakers.domain.interactor.OpenPartnerLinkUseCase
 import fr.androidmakers.domain.interactor.OpenXAccountUseCase
 import fr.androidmakers.domain.interactor.OpenXHashtagUseCase
-import fr.androidmakers.domain.interactor.OpenBlueskyAccountUseCase
 import fr.androidmakers.domain.interactor.OpenYoutubeUseCase
 import fr.androidmakers.domain.interactor.SetSessionBookmarkUseCase
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 expect val domainPlatformModule: Module
 
 val domainModule = module {
-  factory { GetAgendaUseCase(get(), get(), get()) }
-  factory { GetConferenceVenueUseCase(get()) }
-  factory { GetAfterpartyVenueUseCase(get()) }
-  factory { MergeBookmarksUseCase(get(), get()) }
-  factory { OpenFaqUseCase(get()) }
-  factory { OpenCocUseCase(get()) }
-  factory { OpenYoutubeUseCase(get()) }
-  factory { OpenXHashtagUseCase(get()) }
-  factory { OpenBlueskyAccountUseCase(get()) }
-  factory { OpenXAccountUseCase(get()) }
-  factory { SetSessionBookmarkUseCase(get(), get(), get(), get()) }
-  factory { GetPartnersUseCase(get()) }
-  factory { GetFavoriteSessionsUseCase(get()) }
-  factory { OpenPartnerLinkUseCase(get()) }
-  factory { OpenLinkUseCase(get()) }
-  factory { ApplyForAppClinicUseCase(get()) }
+  factoryOf(::GetAgendaUseCase)
+  factoryOf(::GetConferenceVenueUseCase)
+  factoryOf(::GetAfterpartyVenueUseCase)
+  factoryOf(::MergeBookmarksUseCase)
+  factoryOf(::OpenFaqUseCase)
+  factoryOf(::OpenCocUseCase)
+  factoryOf(::OpenYoutubeUseCase)
+  factoryOf(::OpenXHashtagUseCase)
+  factoryOf(::OpenBlueskyAccountUseCase)
+  factoryOf(::OpenXAccountUseCase)
+  factoryOf(::SetSessionBookmarkUseCase)
+  factoryOf(::GetPartnersUseCase)
+  factoryOf(::GetFavoriteSessionsUseCase)
+  factoryOf(::OpenPartnerLinkUseCase)
+  factoryOf(::OpenLinkUseCase)
+  factoryOf(::ApplyForAppClinicUseCase)
 }
--- wearApp/src/main/java/fr/paug/androidmakers/wear/di/DataModule.kt
+++ wearApp/src/main/java/fr/paug/androidmakers/wear/di/DataModule.kt
@@ -1,8 +1,9 @@
 package fr.paug.androidmakers.wear.di
 
 import fr.paug.androidmakers.wear.data.LocalPreferencesRepository
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val dataModule = module {
-  single<LocalPreferencesRepository> { LocalPreferencesRepository(get()) }
+  singleOf(::LocalPreferencesRepository)
 }
