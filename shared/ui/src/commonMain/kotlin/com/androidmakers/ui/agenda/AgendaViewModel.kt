13fc1fae1d7a8e76dfbc7023afd90afe7c10ba0d
BoD <BoD@JRAF.org>
2026-03-22 20:28:42 +0100

    Reload bookmarks when pull to refresh

--- shared/ui/src/commonMain/kotlin/com/androidmakers/ui/agenda/AgendaViewModel.kt
+++ shared/ui/src/commonMain/kotlin/com/androidmakers/ui/agenda/AgendaViewModel.kt
@@ -8,9 +8,11 @@ import com.androidmakers.ui.model.Lce
 import com.androidmakers.ui.model.UISession
 import fr.androidmakers.domain.interactor.ApplyForAppClinicUseCase
 import fr.androidmakers.domain.interactor.GetAgendaUseCase
+import fr.androidmakers.domain.interactor.MergeBookmarksUseCase
 import fr.androidmakers.domain.interactor.SetSessionBookmarkUseCase
 import fr.androidmakers.domain.model.Room
 import fr.androidmakers.domain.repo.BookmarksRepository
+import fr.androidmakers.domain.repo.UserRepository
 import fr.androidmakers.domain.utils.UrlOpener
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -18,18 +20,29 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 class AgendaViewModel(
   getAgendaUseCase: GetAgendaUseCase,
+  mergeBookmarksUseCase: MergeBookmarksUseCase,
   private val setSessionBookmarkUseCase: SetSessionBookmarkUseCase,
   bookmarksRepository: BookmarksRepository,
+  userRepository: UserRepository,
   private val applyForAppClinicUseCase: ApplyForAppClinicUseCase,
 ) : LceViewModel<AgendaState>(
   produce = { refresh ->
     getAgendaUseCase(refresh)
-      .combine(bookmarksRepository.favoriteSessions) { agendaResult, favoriteSessions ->
+      .combine(
+        flow {
+          userRepository.currentUser?.let {
+            mergeBookmarksUseCase(it.id)
+          }
+          emitAll(bookmarksRepository.favoriteSessions)
+        }
+      ) { agendaResult, favoriteSessions ->
         agendaResult.map { agenda ->
           AgendaState(
             days = agendaToDays(agenda, favoriteSessions),
